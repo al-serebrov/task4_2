1434b70d2958501e632fb1d8b168fa12275b6316
Alexander Serebrov <serebrov.alexandr@gmail.com>
2018-04-01 13:20:54 +0300

    Correct issues

--- ntp_deploy.sh
+++ ntp_deploy.sh
@@ -3,15 +3,17 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Install ntp server package
-if [ ! $(dpkg -s ntp | grep Status | cut -d" " -f4) == "installed" ]; then
+if [ -z "$(apt -qq --installed list ntp)" ]; then
   sudo apt-get update
   sudo apt-get -y install ntp
   sudo apt-get -y install git
 fi
 
-if [ ! -d /etc/.git/ ]; then
-  cd /etc && git init && git add ntp.conf && git commit --untracked-files=no -m "Fixing state of ntp.conf"
+# check if sendmail installed, if not - install it
+if [ -z "$(apt -qq --installed list sendmail)" ]; then
+  sudo apt-get -y install sendmail
 fi
+
 # replace the first default server to "ua.pool.ntp.org"
 sudo sed -i "s/0.ubuntu.pool.ntp.org/ua.pool.ntp.org/" /etc/ntp.conf
 
@@ -20,13 +22,19 @@ sudo sed -i "s/pool 1.ubuntu.pool.ntp.org iburst//" /etc/ntp.conf
 sudo sed -i "s/pool 2.ubuntu.pool.ntp.org iburst//" /etc/ntp.conf
 sudo sed -i "s/pool 3.ubuntu.pool.ntp.org iburst//" /etc/ntp.conf
 
+# Commit changed state
+if [ ! -d /etc/.git/ ]; then
+	pushd /etc
+	git init
+	git config user.email "serebrov.alexandr@gmail.com"
+	git config user.name "Alexander Serebrov"
+	git add ntp.conf
+	git commit --untracked-files=no -m "Fixing state of ntp.conf"
+	popd
+fi
+
 # restart ntp service
 sudo /etc/init.d/ntp restart
 
 #write out current crontab
 cd $DIR && touch mycron && echo "* * * * * bash $DIR/ntp_verify.sh" >> mycron && crontab mycron && rm mycron
-#echo new cron into cron file
-
-#install new cron file
-
-
--- ntp_verify.sh
+++ ntp_verify.sh
@@ -1,19 +1,17 @@
 # /bin/bash
 
-if [ ! $(dpkg -s sendmail | grep Status | cut -d" " -f4) == "installed" ]; then
-  sudo apt-get -y install sendmail
-fi
-
 if [ $(sudo service ntp status | grep Active | cut -d" " -f5) == "inactive" ]; then
-  sudo service ntp restart
+	echo "NOTICE: ntp is not running"
+	sudo service ntp restart
 fi
 
 diff=$(cd /etc && git diff ntp.conf)
 
 if [ -n "${diff/[ ]*\n/}" ]; then
   echo "NOTICE: /etc/ntp.conf was changed. Calculated diff:"
-  echo "$(cd /etc && git diff ntp.conf | cut -d$'\n' -f3-)"
-  $(cd /etc && git checkout HEAD ntp.conf)
+  pushd /etc
+  echo "$(git diff ntp.conf | cut -d$'\n' -f3-)"
+  $(git checkout ^HEAD ntp.conf)
   sudo service ntp restart
 else
   exit 0
